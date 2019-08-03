13a7b70149754017eacb5c946e273087a1894e95
Sergey Chirva <schirva@mail.ru>
2019-08-03 12:19:20 +0300

    Print selected items script v2.

---
+++ FilterSelectedItems/ShowSelectedItems v2.js
@@ -0,0 +1,17 @@
+< script type = "text/javascript" >
+
+  SP.SOD.executeOrDelayUntilScriptLoaded('sp.js', 'SP.ClientContext', ShowSelectedItems);
+
+function ShowSelectedItems() {
+  var ctx = new SP.ClientContext.get_current();
+  var items = SP.ListOperation.Selection.getSelectedItems(ctx);
+
+  var mySelected = '';
+
+  for (var i in items) {
+    mySelected += items[i].id + '|';
+  }
+
+  console.log(mySelected);
+} <
+/script>
